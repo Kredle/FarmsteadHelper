ea8c90b78ed4aa13c371b07b3f0dfdff170915ee
Oleh <157129891+Kredle@users.noreply.github.com>
2024-12-18 20:18:07 +0200

    Update urls.py . Added animals path

--- farmstead_farmer/farmstead_farmer/urls.py
+++ farmstead_farmer/farmstead_farmer/urls.py
@@ -3,5 +3,6 @@ from feedback.views import feedback_view
 
 urlpatterns = [
     path('', include('api.urls')),
-    path('feedback/', feedback_view, name='feedback')
+    path('feedback/', feedback_view, name='feedback'),
+    path('animals/', include('animals.urls'))
 ]
