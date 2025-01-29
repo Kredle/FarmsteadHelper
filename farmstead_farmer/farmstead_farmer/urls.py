96b9362177999d8c3b850c815595eb167a817b47
Andriy Kravchuk <andriikravchuk.education@gmail.com>
2025-01-29 15:12:09 +0200

    Update urls.py

--- farmstead_farmer/farmstead_farmer/urls.py
+++ farmstead_farmer/farmstead_farmer/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path, include
 from feedback.views import feedback_view
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path('', include('api.urls')),
@@ -13,3 +18,11 @@ urlpatterns = [
     path('forum/', include('forum.urls')),
     path('map/', include('interactive_map.urls')),
 ]
+
+from django.conf.urls import handler400, handler403, handler404, handler500
+from .views import error_page
+
+handler400 = lambda request, exception: error_page(request, exception, 400)
+handler403 = lambda request, exception: error_page(request, exception, 403)
+handler404 = lambda request, exception: error_page(request, exception, 404)
+handler500 = lambda request: error_page(request, None, 500)
