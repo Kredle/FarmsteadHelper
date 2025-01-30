45c4880a2f4ea71e7f4d3e3b65f50adb54533e1c
Andriy Kravchuk <andriikravchuk.education@gmail.com>
2025-01-30 12:25:12 +0200

    Update urls.py

--- farmstead_farmer/farmstead_farmer/urls.py
+++ farmstead_farmer/farmstead_farmer/urls.py
@@ -5,24 +5,24 @@ from django.conf.urls.static import static
 from django.urls import path
 from django.views.generic import TemplateView
 
+from django.urls import path
+from .views import trigger_400, trigger_403, trigger_404, trigger_500
 
 urlpatterns = [
-    path('', include('api.urls')),
     path('feedback/', feedback_view, name='feedback'),
     path('catalog/animals/', include('animals.urls')),
     path('catalog/trees/', include('trees.urls')),
-    path('', include('main_page.urls')),
     path('catalog/vegetables/', include('vegetables.urls')),
     path('catalog/flowers/', include('plants.urls')),
-    path('', include('catalog.urls')),
     path('forum/', include('forum.urls')),
     path('map/', include('interactive_map.urls')),
+    path('', include('main_page.urls')),  # Загальний шлях для головної сторінки
+    path('', include('api.urls')),       # Загальний шлях для API
+    path('', include('catalog.urls')),   # Загальний шлях для каталогу
+    path("400/", trigger_400),
+    path("403/", trigger_403),
+    path("404/", trigger_404),
+    path("500/", trigger_500),
 ]
 
-from django.conf.urls import handler400, handler403, handler404, handler500
-from .views import error_page
 
-handler400 = lambda request, exception: error_page(request, exception, 400)
-handler403 = lambda request, exception: error_page(request, exception, 403)
-handler404 = lambda request, exception: error_page(request, exception, 404)
-handler500 = lambda request: error_page(request, None, 500)
