84b1769bd7b4139df45f5db9c4b7b6f740dbf404
Andriy Kravchuk <andriikravchuk.education@gmail.com>
2025-01-30 12:24:40 +0200

    Update views.py

--- farmstead_farmer/farmstead_farmer/views.py
+++ farmstead_farmer/farmstead_farmer/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 
-def error_page(request, exception=None, error_code=500):
-    error_messages = {
-        400: "Помилка запиту. Синтаксична помилка або некоректні дані в запиті. Будь ласка, перевірте введену інформацію та спробуйте знову.",
-        403: "Доступ заборонений. Ви не маєте прав для перегляду цієї сторінки.",
-        404: "Сторінку не знайдено. Можливо, вона була видалена або ніколи не існувала.",
-        500: "Сталася помилка на сервері. Спробуйте ще раз пізніше.",
-    }
-    
-    message = error_messages.get(error_code, "Невідома помилка. Спробуйте пізніше.")
-    
-    return render(request, "error.html", {"error_code": error_code, "error_message": message})
+from django.http import HttpResponseBadRequest, HttpResponseForbidden, HttpResponseNotFound, HttpResponseServerError
+
+def trigger_400(request):
+    return HttpResponseBadRequest()
+
+def trigger_403(request):
+    return HttpResponseForbidden()
+
+def trigger_404(request):
+    return HttpResponseNotFound()
+
+def trigger_500(request):
+    return HttpResponseServerError()
