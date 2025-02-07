7779085d6a41d45dac4de63a7fabbe01536d919f
Oleh <157129891+Kredle@users.noreply.github.com>
2025-02-07 20:57:47 +0200

    Uploading files without error_page bug

--- farmstead_farmer/api/views.py
+++ farmstead_farmer/api/views.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authtoken.models import Token
-from .serializers import RegisterSerializer, LoginSerializer
+from .serializers import RegisterSerializer, LoginSerializer, UserProfileSerializer
 from django.shortcuts import render, redirect
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
